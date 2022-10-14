18bdf29e9249d210385434b6cea72efe2fed4aed
stambolievv <deyan.stamboliev@gmail.com>
2022-10-14 17:42:29 +0300

    fix: add configuration file

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+  scene: {
+    backgroundColor: '#111',
+  },
+  rays: {
+    amount: 360,
+    length: 1000,
+    angle: [0, 360],
+    color: 'rgba(245, 245, 45, 0.4)',
+    thickens: 1,
+  },
+  line: {
+    thickens: 3,
+  }
+};
+
+export default config;
