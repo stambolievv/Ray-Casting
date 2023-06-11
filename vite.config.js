f0c70998ef461a41e9957939614e3035d3c5c942
Deyan Stamboliev <deyan.stamboliev@gmail.com>
2023-06-11 18:26:01 +0300

    chore: cleanup and handle project for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import viteBanner from 'vite-plugin-banner';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import { visualizer } from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
 const banner = `
@@ -15,53 +13,37 @@ const banner = `
  * @license ${pkg.license}
  */
 `.trim();
-const root = '.'; /* Project root directory (where index.html is located). */
-const outputFolder = 'dist'; /* Specify the output directory (relative to project root). */
-const assetFolder = 'assets'; /* Referenced in source code. They are hashed. */
-const publicFolder = `${assetFolder}/public`; /* Referenced in source code. They are not hashed. */
-const main = pkg.main || 'index.html'; /* Which file watcher to open. */
-const pluginSettings = {
-  viteStaticCopy: { targets: [{ src: publicFolder, dest: assetFolder }] },
-  viteBanner: { outDir: outputFolder, content: banner },
-  createHtmlPlugin: { minify: true, filename: `${main}` },
-  visualizer: { filename: 'chart.html', title: 'App Visualizer', template: 'treemap', gzipSize: true, brotliSize: true },
-};
+const outputFolder = 'dist'; // Specify the output directory (relative to project root).
+const assetsFolder = 'assets'; // Specify the assets folder (relative to project root).
+const isForGithub = process.env.BRANCH === 'gh-pages';
 
 export default defineConfig({
-  root,
+  base: isForGithub ? '/Ray-Casting/' : '/', // The name of the Github repository
+  assetsInclude: assetsFolder,
   server: {
-    // Development
-    cors: true,
     open: true,
+    host: true,
   },
   build: {
-    // Production
     outDir: outputFolder,
-    assetsDir: assetFolder,
+    assetsDir: assetsFolder,
     assetsInlineLimit: 0,
-    target: 'modules',
-    open: `${outputFolder}/${main}`,
+    minify: 'terser',
     emptyOutDir: true,
-    sourcemap: false,
-    minify: true,
-    manifest: false,
-    reportCompressedSize: true,
     rollupOptions: {
       output: {
         dir: outputFolder,
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name]-[hash].js',
-        assetFileNames: ({ name }) => {
-          if (/\.css$/.test(name ?? '')) return `${assetFolder}/styles/[name]-[hash][extname]`;
-          return '[name]-[hash][extname]';
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
+        assetFileNames: ({ name = '' }) => {
+          if (/\.css/.test(name)) return `${assetsFolder}/styles/[name][extname]`;
+          return '[name][extname]';
         }
       }
     }
   },
   plugins: [
-    viteStaticCopy(pluginSettings.viteStaticCopy),
-    viteBanner(pluginSettings.viteBanner),
-    createHtmlPlugin(pluginSettings.createHtmlPlugin),
-    visualizer(pluginSettings.visualizer),
-  ],
+    viteBanner({ outDir: outputFolder, content: banner }),
+    createHtmlPlugin({ minify: true }),
+  ]
 });
