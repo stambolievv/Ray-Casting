f77fc75be9d1f699a27df213b049c5abec521ca5
Deyan Stamboliev <deyan.stamboliev@gmail.com>
2023-06-11 18:33:40 +0300

    fix: use private variable and enhance by using better approaches

--- src/entities/Line.js
+++ src/entities/Line.js
@@ -1,80 +1,152 @@
-import Vector2D from '../utils/Vector2D';
-import { randomHexColor } from '../utils/misc';
+import { Vector2D } from '../utilities';
 
+/**
+ * @classdesc Represents a line in a two-dimensional space.
+ * @class
+ */
 export default class Line {
-  constructor({ position = { x: 0, y: 0 }, length = 300, angle = 0, config = {} } = {}) {
-    this.startPoint = new Vector2D({ x: position.x, y: position.y });
-    this.endPoint = new Vector2D({ x: position.x, y: position.y + length }).rotate(this.startPoint, angle);
-    this.intersectionPoint = this.endPoint.clone(); /* default */
-    this.length = this.startPoint.distance(this.endPoint);
-    this.angle = angle;
-    this.config = config;
-    this.color = this.config.color || randomHexColor();
+  /**
+   * @description The name of the class.
+   * @type {string}
+   */
+  name;
+  /**
+   * @description The custom configuration options related to the class.
+   * @type {object}
+   */
+  #config;
+  /**
+   * @description The start point of the line segment.
+   * @type {Vector2D}
+   */
+  #startPoint;
+  /**
+   * @description The end point of the line segment.
+   * @type {Vector2D}
+   */
+  #endPoint;
+  /**
+   * @description The angle of the line.
+   * @type {number}
+   */
+  #angle;
+  /**
+   * @description The point where two lines meet each other.
+   * @type {Vector2D|undefined}
+   */
+  #intersectionPoint;
+
+  /**
+   * @description Represents a line in a two-dimensional space.
+   * @param {object} [options] - The options for configuring the line.
+   * @param {string} [options.name] - The name of the line.
+   * @param {object} [options.position] - The position of the line.
+   * @param {number} [options.angle] - The angle of the line.
+   * @param {object} [options.config] - The configuration options for the line.
+   */
+  constructor({ name = 'Line', position = { x: 0, y: 0 }, angle = 0, config = {} } = {}) {
+    this.name = name;
+    this.#config = config;
+
+    this.#startPoint = new Vector2D();
+    this.#endPoint = new Vector2D();
+    this.#angle = angle;
+
+    this.update(new Vector2D(position.x, position.y));
   }
 
   /**
-   * @description Draw a line from the start point to the end point, using the color property.
-   * @param {CanvasRenderingContext2D} context - The context of the canvas.
+   * @description Get the start point of the line.
+   * @returns {Vector2D} The start point of the line.
+   * @readonly
+   */
+  get start() {
+    return this.#startPoint;
+  }
+
+  /**
+   * @description Get the end point of the line.
+   * @returns {Vector2D} The end point of the line.
+   * @readonly
+   */
+  get end() {
+    return this.#endPoint;
+  }
+
+  /**
+   * @description Sets or Gets the intersection point where two lines meet each other.
+   */
+  set intersectionPoint(point) {
+    this.#intersectionPoint = point;
+  }
+  get intersectionPoint() {
+    return this.#intersectionPoint;
+  }
+
+  /**
+   * @description Draws the line segment on a canvas context. The line is drawn from the start point to the end point, using the specified color and line width. If a gradient is provided in the configuration, it is used to create a linear gradient for the line.
+   * @param {CanvasRenderingContext2D} context - The canvas rendering context on which to draw the line.
    */
   draw(context) {
-    const { gradient, color = this.color, lineWidth } = this.config;
+    const { gradient, color, lineWidth } = this.#config;
 
-    const linearGradient = context.createLinearGradient(...this.startPoint.toArray(), ...this.endPoint.toArray());
-    gradient && Object.entries(gradient).forEach(([stop, color]) => linearGradient.addColorStop(stop, color));
+    const linearGradient = context.createLinearGradient(this.start.x, this.start.y, this.end.x, this.end.y);
+    gradient && Object.entries(gradient).forEach(([stop, color]) => linearGradient.addColorStop(Number(stop), color));
+
+    const end = this.intersectionPoint ?? this.end;
 
     context.beginPath();
-    context.moveTo(...this.startPoint.toArray());
-    context.lineTo(...this.intersectionPoint.toArray());
+    context.moveTo(this.start.x, this.start.y);
+    context.lineTo(end.x, end.y);
     context.closePath();
+
     context.lineWidth = lineWidth;
     context.strokeStyle = gradient ? linearGradient : color;
     context.stroke();
   }
 
   /**
-   * @description The update function takes a `position` object and sets the startPoint and endPoint properties of the line to the x and y values of the `position` object. In addition, the length of the line is added to the x coordinate of the endPoint, then rotates the endPoint property around the startPoint property by the angle of the object.
-   * @param {object} position - The position of the mouse.
+   * @description Update the line segment with a new position. The function takes a `position` parameter representing the new start position of the line segment. The start point of the line segment is updated to the provided position, effectively changing the position of the line segment. The end point of the line segment is then recalculated based on the new start position, the length, and the angle of the line segment.
+   * @param {Vector2D} position - The position of the mouse.
    */
   update(position) {
-    const { x, y } = position;
+    const { length } = this.#config;
 
-    this.startPoint.set(x, y);
-    this.endPoint.set(x, y + this.length).rotate(this.startPoint, this.angle);
+    this.#startPoint.copy(position);
+    this.#endPoint = this.#startPoint.addScalar(0, length).rotateSelf(this.#startPoint, this.#angle);
   }
 
   /**
-   * @description See if two line segments intersect. This uses the vector cross-product approach.
-   * @param {Line} line - The line to check for intersection with.
-   * @returns {{type: string, point?: Vector2D}} The intersection of two lines.
+   * @description Finds the intersection between two line segments. This uses the vector cross-product approach.
+   * @param {Line} line - The line segment to intersect with.
+   * @returns {Intersection} An object representing the intersection result.
    * @see {@link https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection Line-line intersection} for further information.
    */
   intersection(line) {
-    /* Just a way to make the code more readable. */
-    const x1 = this.startPoint;
-    const y1 = this.endPoint;
-    const x2 = line.startPoint;
-    const y2 = line.endPoint;
+    const startDiff = line.start.subtract(this.start);
+    const endDiff = this.end.subtract(this.start);
+    const lineDiff = line.end.subtract(line.start);
 
-    const numerator = x2.subtract(x1).crossProduct(y1.subtract(x1));
-    const denominator = y1.subtract(x1).crossProduct(y2.subtract(x2));
+    const numerator = startDiff.crossProduct(endDiff);
+    const denominator = endDiff.crossProduct(lineDiff);
 
     /* Checking if the lines are co-linear. */
     if (numerator === 0 && denominator === 0) {
       /* They are co-linear, and so intersect if they have any overlap. */
 
       /* Do they touch? (Are any of the points equal?) */
-      if (x1.equals(x2) || y1.equals(x2) || x1.equals(y2) || y1.equals(y2)) return { type: 'colinear-overlapping' };
+      if (this.start.equals(line.start) || this.end.equals(line.start) || this.start.equals(line.end) || this.end.equals(line.end)) return { type: 'colinear-overlapping' };
 
       /*Are all the point differences in either direction the same sign? */
       const differences = [
-        ...x2.subtract(x1).toArray(),
-        ...x2.subtract(y1).toArray(),
-        ...y2.subtract(x1).toArray(),
-        ...y2.subtract(y1).toArray()
+        startDiff.toArray(),
+        line.start.subtract(this.end).toArray(),
+        line.end.subtract(this.start).toArray(),
+        line.end.subtract(this.end).toArray()
       ];
 
-      if (!differences.every(point => point < 0)) return { type: 'colinear-disjoint' };
-      else return { type: 'colinear-joint' };
+      if (differences.flat().every(point => point < 0)) return { type: 'colinear-joint' };
+      return { type: 'colinear-disjoint' };
     }
 
     /* Checking if the lines are parallel. If they are, then they will never intersect. */
@@ -82,12 +154,28 @@ export default class Line {
 
     /* The intersection point of the lines is found with one of the following values of `t` or `u`, where: */
     const u = numerator / denominator;
-    const t = x2.subtract(x1).crossProduct(y2.subtract(x2)) / denominator;
+    const t = startDiff.crossProduct(lineDiff) / denominator;
 
     /* Checking if the intersection point is within the bounds of the line segments. */
     const fallsWithinLineSegment = t >= 0 && t <= 1 && u >= 0 && u <= 1;
 
-    if (fallsWithinLineSegment) return { type: 'intersection', point: new Vector2D(x1.add(y1.subtract(x1).multiplyScalar(t)).toObject()) };
-    else return { type: 'no-intersection' };
+    if (fallsWithinLineSegment) {
+      const intersectionPoint = this.start.add(endDiff.multiplyScalar(t));
+      return { type: 'intersection', point: intersectionPoint };
+    }
+
+    return { type: 'no-intersection' };
   }
 }
+
+/**
+ * @typedef {object} Intersection - Represents the intersection between two line segments.
+ * @property {string} type The type of intersection result. Possible values are:
+ * - `colinear-overlapping`: The line segments are colinear and overlapping.
+ * - `colinear-disjoint`: The line segments are colinear but disjoint.
+ * - `colinear-joint`: The line segments are colinear and have a joint endpoint.
+ * - `parallel-non-intersecting`: The line segments are parallel and non-intersecting.
+ * - `intersection`: The line segments intersect.
+ * - `no-intersection`: The line segments do not intersect.
+ * @property {Vector2D} [point] The intersection point.
+ */
--- src/entities/Rays.js
+++ src/entities/Rays.js
@@ -1,70 +1,92 @@
 import Line from './Line';
-import { array, isArray, clampBetweenRanges } from '../utils/misc';
+import { Vector2D, createArray, isArray, clampBetweenRanges } from '../utilities';
 
+/**
+ * @classdesc Represents a collection of rays used for raycasting.
+ * @class
+ */
 export default class Rays {
-  constructor({ position = { x: 0, y: 0 }, amount = 360, length = 1000, angle = [0, 360], config = {} } = {}) {
-    /**@type {Array<Line>} */
-    this.lines = array(amount, i => {
-      const configuration = {
-        position,
-        length,
-        angle: clampBetweenRanges(i, [0, amount], angle),
-        config: {
-          gradient: config.gradient,
-          color: config.color,
-          lineWidth: config.lineWidth
-        }
-      };
+  /**
+   * @description The name of the class.
+   * @type {string}
+   */
+  name;
+  /**
+   * @description The custom configuration options related to the class.
+   * @type {object}
+   */
+  #config;
+  /**
+   * @description An array of lines objects representing the rays.
+   * @type {Array<Line>}
+   */
+  #lines;
 
-      return new Line(configuration);
-    });
+  /**
+   * @description Represents a collection of rays used for raycasting.
+   * @param {object} [options] - The options for configuring the Rays object.
+   * @param {string} [options.name] - The name of the Rays object.
+   * @param {object} [options.config] - The custom configuration options for the Rays object.
+   */
+  constructor({ name = 'Rays', config = {} } = {}) {
+    this.name = name;
+    this.#config = config;
+    this.#lines = this.#init();
   }
 
   /**
-   * @description For each line in the lines array, call the draw function on the line.
-   * @param {CanvasRenderingContext2D} context - The context of the canvas.
+   * @description Draws all the rays on a canvas context.
+   * @param {CanvasRenderingContext2D} context - The canvas rendering context on which to draw the rays.
    */
   draw(context) {
-    this.lines.forEach(line => line.draw(context));
+    for (const line of this.#lines) line.draw(context);
   }
 
   /**
-   * @description It loops through each line in the lines array and calls the update function on it.
-   * @param {object} position - The position of the mouse.
+   * @description Updates the position of all the rays.
+   * @param {Vector2D} position - The new position for the rays.
    */
   update(position) {
-    this.lines.forEach(line => line.update(position));
+    for (const line of this.#lines) line.update(position);
   }
 
   /**
-   * @description For each line in the ray, find the closest intersection point with any of the obstacles, and draw the line to that point.
+   * @description Finds the closest intersection point between the rays and the given line(s).
    * @param {Line|Array<Line>} lines - The lines to check for intersections with.
    */
-  intersection(lines) {
-    /**@type {Array<Line>} */
+  intersect(lines) {
     const obstacles = isArray(lines) ? lines : [lines];
 
-    this.lines.forEach(line => {
-      const observer = {
-        closest: null,
-        record: Infinity
-      };
+    for (const line of this.#lines) {
+      let closest = null;
+      let record = Infinity;
 
-      obstacles.forEach(obstacle => {
+      for (const obstacle of obstacles) {
         const intersection = line.intersection(obstacle);
 
         if (intersection.point) {
-          const distance = line.startPoint.distance(intersection.point);
+          const distance = line.start.distance(intersection.point);
 
-          if (distance < observer.record) {
-            observer.record = distance;
-            observer.closest = intersection.point;
+          if (distance < record) {
+            record = distance;
+            closest = intersection.point;
           }
         }
 
-        if (observer.closest) line.intersectionPoint = observer.closest;
-        else line.intersectionPoint = line.endPoint.clone();
-      });
-    });
+        line.intersectionPoint = closest ? closest : line.end.clone();
+      }
+    }
+  }
+
+  /**
+   * @description Initializes the rays based on the configuration options.
+   * @returns {Array<Line>} An array of Line objects representing the rays.
+   */
+  #init() {
+    const { amount = 0, angle, singleLine } = this.#config;
+
+    return createArray(amount, i => (
+      new Line({ name: `Ray${i}`, angle: clampBetweenRanges(i, [0, amount], angle), config: singleLine })
+    ));
   }
 }
